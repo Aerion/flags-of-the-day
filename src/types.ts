c4e15f65b2f262f40bc0b567b03717168cd0be36
Guillaume Taquet Gasperini <perso@aerion.me>
2025-12-27 14:46:18 +0100

    🩹 Exclude duplicated flags
    Some flags (e.g. Mayotte) are identical in the flag-icons library.
    Thus, remove them to keep it playable for the users.
    
    Can be reproduced with
    `find node_modules/flag-icons/flags/4x3 -name "*.svg" -type f -exec sh -c 'tail -n +2 "$1" | sha1sum' _ {} \; | sort | uniq -c | sort -n`

--- src/types.ts
+++ src/types.ts
@@ -12,9 +12,27 @@ export interface FlagData {
   code: string
 }
 
-// Generate FLAGS array from i18n-iso-countries
+// Territories to exclude: only those that share the same flag content with their parent countries
+// Based on hashes of the svg flags
+const EXCLUDED_CODES = new Set([
+  // French territories sharing flag with France (FR)
+  'BL', // Saint Barthélemy
+  'GF', // French Guiana
+  'GP', // Guadeloupe
+  'MF', // Saint Martin
+  'PM', // Saint Pierre and Miquelon
+  'RE', // Réunion
+  'WF', // Wallis and Futuna
+  'YT', // Mayotte
+  // Other territories sharing flags with parent countries
+  'HM', // Heard Island and McDonald Islands (shares flag with AU)
+  'SH', // Saint Helena (shares flag with GB)
+  'DG', // Diego Garcia (shares flag with IO)
+])
+
 const countryCodes = Object.keys(countries.getNames('en', { select: 'official' }))
 export const FLAGS: FlagData[] = countryCodes
+  .filter((code: string) => !EXCLUDED_CODES.has(code))
   .map((code: string) => {
     const country = countries.getName(code, 'en') || ''
     const countryFr = countries.getName(code, 'fr') || ''
